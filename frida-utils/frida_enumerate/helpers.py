f4510ffaa8b8d5c2a04e950cac55535b4b56bd54
ldzzz <eldinabdic47@gmail.com>
2021-06-08 01:28:41 +0200

    commit incase house burns down

--- frida-utils/frida_enumerate/helpers.py
+++ frida-utils/frida_enumerate/helpers.py
@@ -11,11 +11,11 @@ class MessageCode(Enum):
 class PrinterManager():
     def __init__(self):
         self._color_palette = [Fore.CYAN, Fore.MAGENTA, Fore.YELLOW, Fore.GREEN, Fore.RED, Fore.BLUE]
-        self._next_color = 0
+        self._next_color = 2
 
     def _get_color(self):
         chosen_color = self._color_palette[self._next_color % len(self._color_palette)]
-        self._next_color += 1
+        #self._next_color += 1
         return chosen_color
 
     def print_msg(self, s, event=None):
